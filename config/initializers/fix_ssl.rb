a340f7234f61ee4da5bb9b916b561c5cadd46fa2
Owen <owenperedo@gmail.com>
2015-08-29 06:33:08 -0400

    ssl fix changed

--- config/initializers/fix_ssl.rb
+++ config/initializers/fix_ssl.rb
@@ -5,10 +5,15 @@ module Net
   class HTTP
     alias_method :original_use_ssl=, :use_ssl=
 
+    # fix ssl for facebook connection
     def use_ssl=(flag)
-      self.ca_file = Rails.root.join('lib/ca-bundle.crt').to_s
-      self.verify_mode = OpenSSL::SSL::VERIFY_PEER
-      self.original_use_ssl = flag
+      if @address.include?("facebook.com")
+        self.ca_file = Rails.root.join('lib/ca-bundle.crt').to_s
+        self.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        self.original_use_ssl = flag
+      else
+        super
+      end
     end
   end
 end
