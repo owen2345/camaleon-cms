52c676c012078ce290bdbe44762c0eecb22d2ed9
Owen <owenperedo@gmail.com>
2015-09-03 05:18:38 -0400

    fix rufus initializer

--- config/initializers/rufus_cron.rb
+++ config/initializers/rufus_cron.rb
@@ -6,22 +6,31 @@
   This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   See the  GNU Affero General Public License (GPLv3) for more details.
 =end
-require 'rufus-scheduler'
-$scheduler = Rufus::Scheduler.singleton
-$scheduler.cron '00 05 * * *' do
-  system("rake camaleon_cms:sitemap")
-end
-# cronjob for hook by site
 begin
-  Site.all.each do |site|
-    # hooks
-    c = CamaleonController.new
-    c.instance_eval do
-      @current_site = site
-      @_hooks_skip = []
-    end
-    r = {site: site, eval: nil}; c.hooks_run("cron", r)
-    r[:eval].call(r) if r[:eval].present? # evaluate the cron job created by plugin or theme
+  require 'rufus-scheduler'
+  loaded_rufus = true
+rescue LoadError
+  loaded_rufus = false
+end
+
+if loaded_rufus
+  $scheduler = Rufus::Scheduler.singleton
+  $scheduler.cron '00 05 * * *' do
+    system("rake camaleon_cms:sitemap")
   end
-rescue ActiveRecord::RecordNotFound # skipping sites not found
+
+  # run all custom cron jobs
+  begin
+    Site.all.each do |site|
+      # hooks
+      c = CamaleonController.new
+      c.instance_eval do
+        @current_site = site
+        @_hooks_skip = []
+      end
+      r = {site: site, eval: nil}; c.hooks_run("cron", r)
+      r[:eval].call(r) if r[:eval].present? # evaluate the cron job created by plugin or theme
+    end
+  rescue  #ActiveRecord::RecordNotFound # skipping pending migrations
+  end
 end
--- lib/camaleon_cms/version.rb
+++ lib/camaleon_cms/version.rb
@@ -1,3 +1,3 @@
 module CamaleonCms
-  VERSION = "0.1.7"
+  VERSION = "0.1.9"
 end
