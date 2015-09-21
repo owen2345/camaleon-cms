a23b7e110d0bcf5a7668fe805b74ce0c0b1f30c5
Owen <owenperedo@gmail.com>
2015-09-21 15:00:09 -0400

    removed non used file

--- app/models/meta.rb
+++ app/models/meta.rb
@@ -8,7 +8,6 @@
 =end
 class Meta < ActiveRecord::Base
   self.table_name = "metas"
-  include MetasSaved
   attr_accessible :objectid, :key, :value, :object_class
   #belongs_to :parent, :foreign_key => :objectid
 
