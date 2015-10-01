8cfe56ee6756336008bad87ce000376851a5a28f
Owen <owenperedo@gmail.com>
2015-10-01 09:33:09 -0400

    removed non used comments

--- lib/generators/cplugin_template/app/apps/plugins/my_plugin/models/my_plugin.rb
+++ lib/generators/cplugin_template/app/apps/plugins/my_plugin/models/my_plugin.rb
@@ -5,8 +5,3 @@
   # here create your models normally
   # notice: your tables in database will be plugins_pluginKey in plural (check rails documentation)
 # end
-
-# here your default models customization
-# Site.class_eval do
-#   has_many :pluginKey, class_name: "Plugins::PluginClass::Models::PluginClass"
-# end
