25e84f3e6a4947db82b8c2a89d1b4518439f6635
Owen <owenperedo@gmail.com>
2015-11-11 16:24:31 -0400

    fixed sitemap format for google

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,25 +14,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-# Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
-
-# Rails.application.config.assets.precompile += Dir[Rails.root.join("app", "apps", "themes", "*", "assets", "**", "^(?!_)*.{js,css,png,jpg,gif}")]
-# Rails.application.config.assets.precompile += Dir[Rails.root.join("app", "apps", "plugins", "*", "assets", "**", "^(?!_)*.{js,css,png,jpg,gif}")]
-#
-# Rails.application.config.assets.precompile += Dir[File.join($camaleon_engine_dir, "app", "apps", "themes", "*", "assets", "**", "^(?!_)*.{js,css,png,jpg,gif}")]
-# Rails.application.config.assets.precompile += Dir[File.join($camaleon_engine_dir, "app", "apps", "plugins", "*", "assets", "**", "^(?!_)*.{js,css,png,jpg,gif}")]
-
-# Rails.application.config.assets.precompile += %w( themes/*/assets/css/^(?!_)* )
-# Rails.application.config.assets.precompile += %w( plugins/*/assets/js/^(?!_)* )
-# Rails.application.config.assets.precompile += %w( themes/*/assets/[images|img]/* )
-# Rails.application.config.assets.precompile += %w( plugins/*/assets/[images|img]/* )
-# Rails.application.config.assets.precompile += %w( plugins/*/assets/* )
-
 # This will precompile any assets, not just JavaScript (.js, .coffee, .swf, .css, .scss)
-# Rails.application.config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
 Rails.application.config.assets.precompile << Proc.new { |path|
   name = File.basename(path)
   content_type = MIME::Types.type_for(name).first.content_type rescue ""
--- lib/camaleon_cms/version.rb
+++ lib/camaleon_cms/version.rb
@@ -1,3 +1,3 @@
 module CamaleonCms
-  VERSION = "1.0.8"
+  VERSION = "2.0.0"
 end
