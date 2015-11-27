d5e9c0196b4d922951fcbd0ad8fb662fec64f59c
Owen <owenperedo@gmail.com>
2015-11-27 15:36:36 -0400

    added file path for upload_file(...)

--- app/helpers/camaleon_cms/uploader_helper.rb
+++ app/helpers/camaleon_cms/uploader_helper.rb
@@ -213,7 +213,7 @@ module CamaleonCms::UploaderHelper
 
   # parse file information of FOG file
   def cama_uploader_parse_file(file)
-    res = {"name"=> File.basename(file.key), "size"=> file.content_length, "url"=> (file.public_url rescue [current_site.get_option("filesystem_s3_endpoint"), file.key ].join("/")), "deleteUrl"=> "" }
+    res = {"name"=> File.basename(file.key), "file"=> file.key, "size"=> file.content_length, "url"=> (file.public_url rescue [current_site.get_option("filesystem_s3_endpoint"), file.key ].join("/")), "deleteUrl"=> "" }
     ext = File.extname(file.key).sub(".", "").downcase
     res["format"] = "unknown"
     if "jpg,jpeg,png,gif,bmp,ico".split(",").include?(ext)
