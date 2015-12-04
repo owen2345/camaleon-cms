ccc42c3dfc0e61ddfe735af7c89d7e20848e7b8f
Martin Meyer <mmeyer@altimetrik.com>
2015-12-04 10:48:05 -0800

    Added namespace to @current_user
    Added namespace cama to avoid conflicts with other gems/apps when its installed on an already existing project

--- app/helpers/camaleon_cms/session_helper.rb
+++ app/helpers/camaleon_cms/session_helper.rb
@@ -117,17 +117,17 @@ module CamaleonCms::SessionHelper
 
   # return current user logged in
   def cama_current_user
-    return @current_user if defined?(@current_user)
+    return @cama_current_user if defined?(@cama_current_user)
     # api current user...
-    @current_user = cama_calc_api_current_user
-    return @current_user unless @current_user.nil?
+    @cama_current_user = cama_calc_api_current_user
+    return @cama_current_user unless @cama_current_user.nil?
 
     return nil unless cookies[:auth_token].present?
     c = cookies[:auth_token].split("&")
     return nil unless c.size == 3
 
     if c[1] == request.user_agent && request.ip == c[2]
-      @current_user = (current_site.users_include_admins.find_by_auth_token(c[0]).decorate rescue nil)
+      @cama_current_user = (current_site.users_include_admins.find_by_auth_token(c[0]).decorate rescue nil)
     end
   end
 
