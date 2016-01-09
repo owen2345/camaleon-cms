ee482f088edae0e22329d7ea7e75883c8649d4d2
Owen <owenperedo@gmail.com>
2016-01-09 07:15:44 -0400

    added model class name aliases now you can call by class alias like: Cama::Site.first

---
+++ config/initializers/model_alias.rb
@@ -0,0 +1,10 @@
+module Cama
+end
+Cama::Site = CamaleonCms::Site
+Cama::Post = CamaleonCms::Post
+Cama::Category = CamaleonCms::Category
+Cama::PostTag = CamaleonCms::PostTag
+Cama::PostType = CamaleonCms::PostType
+Cama::TermTaxonomy = CamaleonCms::TermTaxonomy
+Cama::TermRelationship = CamaleonCms::TermRelationship
+
