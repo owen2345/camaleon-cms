d8475dd365bae82136de7dc23e40b61f69b170e6
Owen Peredo <owenperedo@gmail.com>
2016-06-12 15:41:17 -0300

    fixed theme paths after switch theme

--- app/controllers/camaleon_cms/frontend_controller.rb
+++ app/controllers/camaleon_cms/frontend_controller.rb
@@ -226,6 +226,7 @@ class CamaleonCms::FrontendController < CamaleonCms::CamaleonController
     lookup_context.prefixes.delete("camaleon_cms/camaleon")
     lookup_context.prefixes.delete("camaleon_cms/apps/plugins_front")
     lookup_context.prefixes.delete("camaleon_cms/apps/themes_front")
+    lookup_context.prefixes.delete_if{|t| t =~ /themes\/(.*)\/views/i || t == "camaleon_cms/default_theme" || t == "themes/#{current_site.id}/views" }
 
     lookup_context.prefixes.append("themes/#{current_site.id}/views") if Dir.exist?(Rails.root.join('app', 'apps', 'themes', current_site.id.to_s).to_s)
     lookup_context.prefixes.append("themes/#{current_theme.slug}/views")
