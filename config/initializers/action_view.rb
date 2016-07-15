9dc5d92ed670d38965265229f410cfa3c33f5418
Owen Peredo <owenperedo@gmail.com>
2016-07-15 16:55:49 -0400

    - fixed view paths

--- config/initializers/action_view.rb
+++ config/initializers/action_view.rb
@@ -25,17 +25,41 @@ module ActionView
         @view_paths.find(*args_for_lookup(name, prefixes, partial, keys, options))
       end
       alias :find_template :find
-
-      # fix to add camaleon prefixes on verify template exist
-      def exists?(name, prefixes = [], partial = false, keys = [], options = {})
-        if use_camaleon_partial_prefixes.present?
-          prefixes = [""] unless prefixes.present?
-          prefixes = (prefixes+self.prefixes).uniq if prefixes.is_a?(Array)
-        end
-        @view_paths.exists?(*args_for_lookup(name, prefixes, partial, keys, options))
-      end
-      alias :template_exists? :exists?
     end
   end
 end
-# if Rails.version.to_s[0].to_i == 4
+
+
+if Rails.version.to_s[0].to_i == 4
+  module ActionView
+    class LookupContext #:nodoc:
+      module ViewPaths
+        # fix to add camaleon prefixes on verify template exist
+        def exists?(name, prefixes = [], partial = false, keys = [], options = {})
+          if use_camaleon_partial_prefixes.present?
+            prefixes = [""] unless prefixes.present?
+            prefixes = (prefixes+self.prefixes).uniq if prefixes.is_a?(Array)
+          end
+          @view_paths.exists?(*args_for_lookup(name, prefixes, partial, keys, options))
+        end
+        alias :template_exists? :exists?
+      end
+    end
+  end
+else
+  module ActionView
+    class LookupContext #:nodoc:
+      module ViewPaths
+        # fix to add camaleon prefixes on verify template exist
+        def exists?(name, prefixes = [], partial = false, keys = [], **options)
+          if use_camaleon_partial_prefixes.present?
+            prefixes = [""] unless prefixes.present?
+            prefixes = (prefixes+self.prefixes).uniq if prefixes.is_a?(Array)
+          end
+          @view_paths.exists?(*args_for_lookup(name, prefixes, partial, keys, options))
+        end
+        alias :template_exists? :exists?
+      end
+    end
+  end
+end
