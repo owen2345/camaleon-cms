2fc64b927e4f2890d2411f3564f3ad368e1bce80
Owen Peredo <owenperedo@gmail.com>
2016-08-03 07:16:10 -0400

    - changed lincese into MIT - created version 2.3.0 with rails 5 support

--- app/apps/plugins/authoring_post/authoring_post_helper.rb
+++ app/apps/plugins/authoring_post/authoring_post_helper.rb
@@ -1,14 +1,3 @@
-=begin
-  Camaleon CMS is a content management system
-  Copyright (C) 2015 by Owen Peredo Diaz
-  Email: owenperedo@gmail.com
-  Authoring Post Plugin
-  Copyright (C) 2016 by Second Bureau
-  Email: gilles@secondbureau.com
-  This program is free software: you can redistribute it and/or modify   it under the terms of the GNU Affero General Public License as  published by the Free Software Foundation, either version 3 of the  License, or (at your option) any later version.
-  This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the  GNU Affero General Public License (GPLv3) for more details.
-=end
 module Plugins::AuthoringPost::AuthoringPostHelper
   
   def plugin_authoring_post_the_content(args)
