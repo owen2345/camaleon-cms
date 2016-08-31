068cf0c3c7f5f88f8f9eaa62a68426e08d918d70
Oleg Pudeyev <oleg@bsdpower.com>
2016-08-31 10:45:46 -0400

    Can drop unique slug workaround

--- spec/routing/post_type_routes_spec.rb
+++ spec/routing/post_type_routes_spec.rb
@@ -2,12 +2,11 @@ require "rails_helper"
 
 RSpec.describe "routes for Post Types", :type => :routing do
   it "routes for /test-post-type" do
-    slug = "test-post-type-#{rand.to_s[2..10]}"
-    post_type = create_test_post_type(slug: slug)
+    post_type = create_test_post_type(slug: "test-post-type")
     expect(post_type.id).to be_present
-    expect(get("/#{slug}")).to route_to(
+    expect(get("/test-post-type")).to route_to(
       "camaleon_cms/frontend#post_type",
       post_type_id: post_type.id,
-      post_type_slug: slug)
+      post_type_slug: "test-post-type")
   end
 end
