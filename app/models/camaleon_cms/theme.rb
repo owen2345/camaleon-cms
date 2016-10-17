b0394bc6e4920fa8ebb6636b9dc89b4b391b3903
sabina hofmann <sabinahofmann@gmx.de>
2016-10-17 18:01:30 +0200

    cleanup

--- app/models/camaleon_cms/theme.rb
+++ app/models/camaleon_cms/theme.rb
@@ -1,31 +1,32 @@
 class CamaleonCms::Theme < CamaleonCms::TermTaxonomy
   # attrs:
-  #   slug => plugin key
-  has_many :metas, -> { where(object_class: 'Theme') }, class_name: "CamaleonCms::Meta", foreign_key: :objectid, dependent: :destroy
-  belongs_to :site, class_name: "CamaleonCms::Site", foreign_key: :parent_id
-
+  # slug => plugin key
   default_scope { where(taxonomy: :theme) }
+
+  has_many :metas, -> { where(object_class: 'Theme') }, class_name: 'CamaleonCms::Meta',
+    foreign_key: :objectid, dependent: :destroy
+  belongs_to :site, class_name: 'CamaleonCms::Site', foreign_key: :parent_id
 
   before_validation :fix_name
   before_destroy :destroy_custom_fields
 
   # return theme settings configured in config.json
   def settings
-    PluginRoutes.theme_info(self.slug)
+    PluginRoutes.theme_info(slug)
   end
 
   # return the path to the settings file for current theme
   def settings_file
-    File.join(self.settings["path"], "views/admin/settings.html.erb").to_s
+    File.join(settings["path"], "views/admin/settings.html.erb").to_s
   end
 
   private
+
   def fix_name
-    self.name = self.slug unless self.name.present?
+    name = slug unless name.present?
   end
 
   def destroy_custom_fields
-    self.get_field_groups.destroy_all
+    get_field_groups.destroy_all
   end
-
 end
