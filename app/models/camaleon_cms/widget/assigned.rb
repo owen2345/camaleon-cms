d4432de316d98dd551169217a7508457860240a6
sabina hofmann <sabinahofmann@gmx.de>
2016-10-17 19:09:27 +0200

    cleanup

--- app/models/camaleon_cms/widget/assigned.rb
+++ app/models/camaleon_cms/widget/assigned.rb
@@ -1,5 +1,6 @@
 class CamaleonCms::Widget::Assigned < CamaleonCms::PostDefault
   default_scope ->{ where(post_class: self.name).order(:taxonomy_id) }
+
   # post_parent: sidebar_id
   # visibility: widget_id
   # comment_count: item_order
@@ -10,18 +11,20 @@ class CamaleonCms::Widget::Assigned < CamaleonCms::PostDefault
 
   # attr_accessible :widget_id, :sidebar_id, :item_order
 
-  has_many :metas, ->{ where(object_class: 'Widget::Assigned')}, :class_name => "CamaleonCms::Meta", foreign_key: :objectid, dependent: :destroy
-  belongs_to :sidebar, class_name: "CamaleonCms::Widget::Sidebar", foreign_key: :post_parent
-  belongs_to :widget, class_name: "CamaleonCms::Widget::Main", foreign_key: :visibility
+  has_many :metas, ->{ where(object_class: 'Widget::Assigned')}, class_name: 'CamaleonCms::Meta',
+    foreign_key: :objectid, dependent: :destroy
+  belongs_to :sidebar, class_name: 'CamaleonCms::Widget::Sidebar', foreign_key: :post_parent
+  belongs_to :widget, class_name: 'CamaleonCms::Widget::Main', foreign_key: :visibility
   after_initialize :fix_slug2
   before_create :set_order
 
   def fix_slug2
-    self.slug = "slug_assigned" unless self.slug.present?
+    slug = 'slug_assigned' unless slug.present?
   end
 
   private
+
   def set_order
-    self.item_order = self.sidebar.assigned.count + 1
+    item_order = sidebar.assigned.count + 1
   end
 end
