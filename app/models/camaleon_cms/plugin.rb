1d1948145687f9d581f83e41abbc73507f1ed274
sabina hofmann <sabinahofmann@gmx.de>
2016-10-17 17:23:59 +0200

    cleanup

--- app/models/camaleon_cms/plugin.rb
+++ app/models/camaleon_cms/plugin.rb
@@ -6,35 +6,36 @@ class CamaleonCms::Plugin < CamaleonCms::TermTaxonomy
 
   attr_accessor :error
 
-  has_many :metas, -> { where(object_class: 'Plugin') }, class_name: "CamaleonCms::Meta", foreign_key: :objectid, dependent: :destroy
-  belongs_to :site, class_name: "CamaleonCms::Site", foreign_key: :parent_id
-
   default_scope { where(taxonomy: :plugin) }
   scope :active, -> { where(term_group: 1) }
+
+  has_many :metas, -> { where(object_class: 'Plugin') }, class_name: 'CamaleonCms::Meta',
+    foreign_key: :objectid, dependent: :destroy
+  belongs_to :site, class_name: 'CamaleonCms::Site', foreign_key: :parent_id
 
   before_validation :set_default
   before_destroy :destroy_custom_fields
 
   # active the plugin
   def active
-    self.term_group = 1
-    self.save
+    term_group = 1
+    save
   end
 
   # inactive the plugin
   def inactive
-    self.term_group = nil
-    self.save
+    term_group = nil
+    save
   end
 
   # check if plugin is active
   def active?
-    self.term_group.to_s == "1"
+    term_group.to_s == '1'
   end
 
   # return theme settings configured in config.json
   def settings
-    PluginRoutes.plugin_info(self.slug)
+    PluginRoutes.plugin_info(slug)
   end
 
   # check if current installation version is older
@@ -46,32 +47,32 @@ class CamaleonCms::Plugin < CamaleonCms::TermTaxonomy
 
   # set a new installation version for this plugin
   def installed_version=(version)
-    self.set_option("version_installed", version)
+    set_option('version_installed', version)
   end
 
   # return gem installed version
   def installed_version
     return ""
-    res = self.get_option("version_installed")
+    res = get_option('version_installed')
     unless res.present? # fix for old installations
-      res = self.settings["version"]
-      self.installed_version= res
+      res = settings['version']
+      installed_version = res
     end
     res
   end
 
   # return the title of this plugin
   def title
-    PluginRoutes.plugin_info(self.slug)["title"]
+    PluginRoutes.plugin_info(slug)['title']
   end
 
   private
+
   def set_default
-    self.name = self.slug unless self.name.present?
+    name = slug unless name.present?
   end
 
   def destroy_custom_fields
-    self.get_field_groups.destroy_all
+    get_field_groups.destroy_all
   end
-
 end
