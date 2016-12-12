7dcf3fa664c389b95f886cfe6183e1e987ae37ec
Owen Peredo <owenperedo@gmail.com>
2016-12-12 07:32:44 -0400

    - Added: the_json_fields, the_json_field attribute for custom fields - Removed clearfix on posts lists (default theme)

--- app/decorators/camaleon_cms/custom_fields_concern.rb
+++ app/decorators/camaleon_cms/custom_fields_concern.rb
@@ -13,11 +13,7 @@ module CamaleonCms::CustomFieldsConcern
   def the_field(field_key, default_val = '')
     h.do_shortcode(object.get_field(field_key, default_val).to_s.translate(@_deco_locale), object)
   end
-
-  # the same as the_field(..), but this return default value if there is not present
-  def the_field!(field_key, default_val = '')
-    h.do_shortcode(object.get_field!(field_key, default_val).to_s.translate(@_deco_locale), object)
-  end
+  alias_method :the_field!, :the_field
 
   # return custom field contents with key field_key
   # translated and short codes evaluated like the content
@@ -26,21 +22,6 @@ module CamaleonCms::CustomFieldsConcern
     r = []
     object.get_fields(field_key).each do |text|
       r << h.do_shortcode(text.to_s.translate(@_deco_locale), object)
-    end
-    r
-  end
-
-  # return custom field contents with key field_key (only for type attributes)
-  # translated and short codes evaluated like the content
-  # this is for multiple values
-  def the_attribute_fields(field_key)
-    r = []
-    object.get_fields(field_key).each do |text|
-      _r = JSON.parse(text || '{}').with_indifferent_access
-      _r.keys.each do |k|
-        _r[k] = h.do_shortcode(_r[k].to_s.translate(@_deco_locale), object)
-      end
-      r << _r
     end
     r
   end
@@ -58,14 +39,31 @@ module CamaleonCms::CustomFieldsConcern
     res
   end
 
+  # return custom field contents with key field_key (only for type attributes)
+  # translated and short codes evaluated like the content
+  # this is for multiple values
+  def the_json_fields(field_key)
+    r = []
+    object.get_fields(field_key).each do |text|
+      _r = JSON.parse(text || '{}').with_indifferent_access
+      _r.keys.each do |k|
+        _r[k] = h.do_shortcode(_r[k].to_s.translate(@_deco_locale), object)
+      end
+      r << _r
+    end
+    r
+  end
+  alias_method :the_attribute_fields, :the_json_fields
+
   # return custom field content with key field_key (only for type attributes)
   # translated and short codes evaluated like the content
   # default_val: default value returned when this field was not registered
-  def the_attribute_field(field_key, default_val = '')
+  def the_json_field(field_key, default_val = '')
     r = JSON.parse(object.get_field(field_key, default_val) || '{}').with_indifferent_access
     r.keys.each do |k|
       r[k] = h.do_shortcode(r[k].to_s.translate(@_deco_locale), object)
     end
     r
   end
+  alias_method :the_attribute_field, :the_json_field
 end
