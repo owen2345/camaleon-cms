0535899a97db50b58779c643c5d6a3b5c17eb49b
Richard Li <richard@beansmile.com>
2017-04-23 21:46:39 +0800

    Fix frontend routes issues

--- config/routes/frontend.rb
+++ config/routes/frontend.rb
@@ -14,18 +14,51 @@ Rails.application.routes.draw do
 
       controller "camaleon_cms/frontend" do
         get :index
-        get ":label/:post_type_id-:title" => :post_type, as: "post_type", constraints: {post_type_id: /[0-9]+/, label: /(#{PluginRoutes.all_translations('routes.group', default: 'group').join('|')})/}
-        get ":label/:post_type_id-:title/:slug" => :post, as: "post_of_post_type", constraints: {post_type_id: /[0-9]+/, label: /(#{PluginRoutes.all_translations('routes.group', default: 'group').join('|')})/}
-        get ":label/:category_id-:title" => :category, as: "category", constraints: {category_id: /[0-9]+/, label: /(#{PluginRoutes.all_translations('routes.category', default: 'category').join('|')})/}
-        get ":label_cat/:category_id-:title/:slug" => :post, as: "post_of_category", constraints: {category_id: /[0-9]+/, label_cat: /(#{PluginRoutes.all_translations('routes.category', default: 'category').join('|')})/}
-        get ":post_type_title/:label_cat/:category_id-:title/:slug" => :post, as: "post_of_category_post_type", constraints:{ post_type_title: /(?!(#{PluginRoutes.all_locales}))[\w\.\-]+/, category_id: /[0-9]+/, label_cat: /(#{PluginRoutes.all_translations('routes.category', default: 'category').join('|')})/ }
-        get ":label/:post_tag_id-:title" => :post_tag, as: "post_tag", constraints: {post_tag_id: /[0-9]+/, label: /(#{PluginRoutes.all_translations('routes.tag', default: 'tag').join('|')})/}
-        get ":label/:post_tag_id-:title/:slug" => :post, as: "post_of_tag", constraints: {post_tag_id: /[0-9]+/, label: /(#{PluginRoutes.all_translations('routes.tag', default: 'tag').join('|')})/}
-        get ":label/:post_tag_slug" => :post_tag, as: "post_tag_simple", constraints: {post_tag_slug: /[a-zA-Z0-9_=\s\-\/]+/, label: /(#{PluginRoutes.all_translations('routes.tag', default: 'tag').join('|')})/}
-        get ":label/:user_id-:user_name" => :profile, as: :profile, defaults:{label: 'profile'}, constraints: {user_id: /[0-9]+/, label: /(#{PluginRoutes.all_translations('routes.profile', default: 'profile').join('|')})/}
-        get ":label" => :search, as: :search, defaults:{label: 'search'}, constraints: {label: /(#{PluginRoutes.all_translations('routes.search', default: 'search').join('|')})/}
+        get ':label/:post_type_id-:title' => :post_type, as: 'post_type', constraints: ->(request) {
+          multilingual_segment = PluginRoutes.all_translations('routes.group', default: 'group')
+          request.params[:post_type_id].match(/[0-9]+/) && request.params[:label].in?(multilingual_segment)
+        }
+        get ':label/:post_type_id-:title/:slug' => :post, as: 'post_of_post_type', constraints: ->(request) {
+          multilingual_segment = PluginRoutes.all_translations('routes.group', default: 'group')
+          request.params[:post_type_id].match(/[0-9]+/) && request.params[:label].in?(multilingual_segment)
+        }
+        get ':label/:category_id-:title' => :category, as: 'category', constraints: ->(request) {
+          multilingual_segment = PluginRoutes.all_translations('routes.category', default: 'category')
+          request.params[:category_id].match(/[0-9]+/) && request.params[:label].in?(multilingual_segment)
+        }
+        get ':label_cat/:category_id-:title/:slug' => :post, as: 'post_of_category', constraints: ->(request) {
+          multilingual_segment = PluginRoutes.all_translations('routes.category', default: 'category')
+          request.params[:category_id].match(/[0-9]+/) && request.params[:label_cat].in?(multilingual_segment)
+        }
+        get ':post_type_title/:label_cat/:category_id-:title/:slug' => :post, as: 'post_of_category_post_type', constraints: ->(request) {
+          multilingual_segment = PluginRoutes.all_translations('routes.category', default: 'category')
+          request.params[:post_type_title].match(/^(?!(#{PluginRoutes.all_locales})$)[\w\.\-]+$/) &&
+            request.params[:category_id].match(/[0-9]+/) && request.params[:label_cat].in?(multilingual_segment)
+        }
+        get ':label/:post_tag_id-:title' => :post_tag, as: 'post_tag', constraints: ->(request) {
+          multilingual_segment = PluginRoutes.all_translations('routes.tag', default: 'tag')
+          request.params[:post_tag_id].match(/[0-9]+/) && request.params[:label].in?(multilingual_segment)
+        }
+        get ':label/:post_tag_id-:title/:slug' => :post, as: 'post_of_tag', constraints: ->(request) {
+          multilingual_segment = PluginRoutes.all_translations('routes.tag', default: 'tag')
+          request.params[:post_tag_id].match(/[0-9]+/) && request.params[:label].in?(multilingual_segment)
+        }
+        get ':label/:post_tag_slug' => :post_tag, as: 'post_tag_simple', constraints: ->(request) {
+          multilingual_segment = PluginRoutes.all_translations('routes.tag', default: 'tag')
+          request.params[:post_tag_slug].match(/[a-zA-Z0-9_=\s\-\/]+/) && request.params[:label].in?(multilingual_segment)
+        }
+        get ':label/:user_id-:user_name' => :profile, as: :profile, defaults:{label: 'profile'}, constraints: ->(request) {
+          multilingual_segment = PluginRoutes.all_translations('routes.profile', default: 'profile') | %w(profile)
+          request.params[:user_id].match(/[0-9]+/) && request.params[:label].in?(multilingual_segment)
+        }
+        get ':label' => :search, as: :search, defaults:{label: 'search'}, constraints: ->(request) {
+          multilingual_segment = PluginRoutes.all_translations('routes.search', default: 'search') | %w(search)
+          request.params[:label].in?(multilingual_segment)
+        }
 
-        get ':post_type_title/:slug' => :post, as: :post_of_posttype, constraints:{ post_type_title: /(?!(#{PluginRoutes.all_locales}))[\w\.\-]+/ }
+        get ':post_type_title/:slug' => :post, as: :post_of_posttype, constraints: ->(request) {
+          request.params[:post_type_title].match(/^(?!(#{PluginRoutes.all_locales})$)[\w\.\-]+$/)
+        }
 
         post 'save_comment/:post_id' => :save_comment, as: :save_comment
         post 'save_form' => :save_form, as: :save_form
@@ -40,14 +73,20 @@ Rails.application.routes.draw do
         controller "camaleon_cms/frontend" do
           PluginRoutes.get_sites.each do |s|
             s.post_types.pluck(:slug, :id).each do |pt_slug, pt_id|
-              get ":post_type_slug" => :post_type, as: "post_type_#{pt_id}", post_type_id: pt_id, constraints: {post_type_slug: /(#{PluginRoutes.all_locales.split("|").map{|_l| I18n.t("routes.post_types.#{pt_slug}", default: pt_slug, locale: _l) }.uniq.join('|') })/}
+              get ':post_type_slug' => :post_type, as: "post_type_#{pt_id}", post_type_id: pt_id, constraints: ->(request) {
+                multilingual_segment = PluginRoutes.all_translations("routes.post_types.#{pt_slug}", default: pt_slug)
+                request.params[:post_type_slug].in?(multilingual_segment)
+              }
             end
           end
         end
 
         # posts
         constraints(format: /html|rss|json/) do
-          get ':parent_title/*slug(.:format)' => :post, as: :hierarchy_post, constraints:{ parent_title: /(?!(#{PluginRoutes.all_locales}))[\w\-]+/, slug: /[a-zA-Z0-9_=\s\-\/]+/ }
+          get ':parent_title/*slug(.:format)' => :post, as: :hierarchy_post, constraints: ->(request) {
+            request.params[:parent_title].match(/^(?!(#{PluginRoutes.all_locales})$)[\w\-]+$/) &&
+              request.params[:slug].match(/[a-zA-Z0-9_=\s\-\/]+/)
+          }
           get ":slug(.:format)" => :post, :as => :post, constraints: { slug: /[a-zA-Z0-9_=\s\-]+/}
         end
       end
