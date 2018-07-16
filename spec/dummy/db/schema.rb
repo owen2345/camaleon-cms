ffb9cbdb554239dc39339f097622e622ba0eadf4
Abd El Rahman Hassan <abdelrahman.hsn@2loops.com>
2018-07-16 15:14:39 +0200

    Add language Arabic

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,87 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2016_05_17_143441) do
+ActiveRecord::Schema.define(version: 2018_07_04_211100) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.string "author_email"
+    t.string "author_url"
+    t.string "author_IP"
+    t.text "content"
+    t.string "approved", default: "pending"
+    t.string "agent"
+    t.string "typee"
+    t.integer "comment_parent"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["approved"], name: "index_comments_on_approved"
+    t.index ["comment_parent"], name: "index_comments_on_comment_parent"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "custom_fields", force: :cascade do |t|
+    t.string "object_class"
+    t.string "name"
+    t.string "slug"
+    t.integer "objectid"
+    t.integer "parent_id"
+    t.integer "field_order"
+    t.integer "count", default: 0
+    t.boolean "is_repeat", default: false
+    t.text "description"
+    t.string "status"
+    t.index ["object_class"], name: "index_custom_fields_on_object_class"
+    t.index ["objectid"], name: "index_custom_fields_on_objectid"
+    t.index ["parent_id"], name: "index_custom_fields_on_parent_id"
+    t.index ["slug"], name: "index_custom_fields_on_slug"
+  end
+
+  create_table "custom_fields_relationships", force: :cascade do |t|
+    t.integer "objectid"
+    t.integer "custom_field_id"
+    t.integer "term_order"
+    t.string "object_class"
+    t.text "value", limit: 1073741823
+    t.string "custom_field_slug"
+    t.integer "group_number", default: 0
+    t.index ["custom_field_id"], name: "index_custom_fields_relationships_on_custom_field_id"
+    t.index ["custom_field_slug"], name: "index_custom_fields_relationships_on_custom_field_slug"
+    t.index ["object_class"], name: "index_custom_fields_relationships_on_object_class"
+    t.index ["objectid"], name: "index_custom_fields_relationships_on_objectid"
+  end
+
+  create_table "media", force: :cascade do |t|
+    t.integer "site_id"
+    t.string "name"
+    t.boolean "is_folder", default: false
+    t.string "folder_path"
+    t.string "file_size"
+    t.string "dimension", default: ""
+    t.string "file_type"
+    t.string "url"
+    t.string "thumb"
+    t.boolean "is_public", default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["folder_path"], name: "index_media_on_folder_path"
+    t.index ["is_folder"], name: "index_media_on_is_folder"
+    t.index ["name"], name: "index_media_on_name"
+    t.index ["site_id"], name: "index_media_on_site_id"
+  end
+
+  create_table "metas", force: :cascade do |t|
+    t.string "key"
+    t.text "value", limit: 1073741823
+    t.integer "objectid"
+    t.string "object_class"
+    t.index ["key"], name: "index_metas_on_key"
+    t.index ["object_class"], name: "index_metas_on_object_class"
+    t.index ["objectid"], name: "index_metas_on_objectid"
+  end
 
   create_table "plugins_contact_forms", force: :cascade do |t|
     t.integer "site_id"
@@ -23,6 +103,84 @@ ActiveRecord::Schema.define(version: 2016_05_17_143441) do
     t.text "settings"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "title"
+    t.text "slug"
+    t.text "content", limit: 1073741823
+    t.text "content_filtered", limit: 1073741823
+    t.string "status", default: "published"
+    t.datetime "published_at"
+    t.integer "post_parent"
+    t.string "visibility", default: "public"
+    t.text "visibility_value"
+    t.string "post_class", default: "Post"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_order", default: 0
+    t.integer "taxonomy_id"
+    t.boolean "is_feature", default: false
+    t.index ["post_class"], name: "index_posts_on_post_class"
+    t.index ["post_parent"], name: "index_posts_on_post_parent"
+    t.index ["slug"], name: "index_posts_on_slug"
+    t.index ["status"], name: "index_posts_on_status"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "term_relationships", force: :cascade do |t|
+    t.integer "objectid"
+    t.integer "term_order"
+    t.integer "term_taxonomy_id"
+    t.index ["objectid"], name: "index_term_relationships_on_objectid"
+    t.index ["term_order"], name: "index_term_relationships_on_term_order"
+    t.index ["term_taxonomy_id"], name: "index_term_relationships_on_term_taxonomy_id"
+  end
+
+  create_table "term_taxonomy", force: :cascade do |t|
+    t.string "taxonomy"
+    t.text "description", limit: 1073741823
+    t.integer "parent_id"
+    t.integer "count"
+    t.text "name"
+    t.string "slug"
+    t.integer "term_group"
+    t.integer "term_order"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["parent_id"], name: "index_term_taxonomy_on_parent_id"
+    t.index ["slug"], name: "index_term_taxonomy_on_slug"
+    t.index ["taxonomy"], name: "index_term_taxonomy_on_taxonomy"
+    t.index ["term_order"], name: "index_term_taxonomy_on_term_order"
+    t.index ["user_id"], name: "index_term_taxonomy_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "role", default: "client"
+    t.string "email"
+    t.string "slug"
+    t.string "password_digest"
+    t.string "auth_token"
+    t.string "password_reset_token"
+    t.integer "parent_id"
+    t.datetime "password_reset_sent_at"
+    t.datetime "last_login_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "site_id", default: -1
+    t.string "confirm_email_token"
+    t.datetime "confirm_email_sent_at"
+    t.boolean "is_valid_email", default: true
+    t.string "first_name"
+    t.string "last_name"
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["role"], name: "index_users_on_role"
+    t.index ["site_id"], name: "index_users_on_site_id"
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
