74ed29f5a6a01e241f4ae59cff8c6d9f98f71b17
Owen <owenperedo@gmail.com>
2019-03-11 10:42:13 -0400

    Improved url in hreflang link

--- app/helpers/camaleon_cms/frontend/seo_helper.rb
+++ app/helpers/camaleon_cms/frontend/seo_helper.rb
@@ -72,7 +72,7 @@ module CamaleonCms::Frontend::SeoHelper
     if l.size > 1
       l.each do |lang|
         s[:alternate] << {
-          href: @object.present? ? @object.the_url(locale: lang) : cama_url_to_fixed("url_for", {locale: lang}),
+          href: options[:object].present? ? options[:object].the_url(locale: lang) : current_site.the_url(locale: lang),
           hreflang: lang
         }
       end
