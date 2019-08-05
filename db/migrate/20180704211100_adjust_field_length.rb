940a3f31bcfbfbd0fa16f8773bd507919a28218b
Owen <owenperedo@gmail.com>
2019-08-05 10:50:42 -0400

    Fixed posts slug index length

--- db/migrate/20180704211100_adjust_field_length.rb
+++ db/migrate/20180704211100_adjust_field_length.rb
@@ -4,7 +4,7 @@ class AdjustFieldLength < CamaManager.migration_class
     change_column post_table, :title, :text
     remove_index(post_table, :slug) if index_exists?(post_table, :slug)
     change_column post_table, :slug, :text
-    add_index post_table, :slug, length: 500
+    add_index post_table, :slug, length: 255
     change_column "#{PluginRoutes.static_system_info["db_prefix"]}term_taxonomy", :name, :text
   end
 end
