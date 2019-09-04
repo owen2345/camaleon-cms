8c6eefb1596e3226162b4f2c3b26d9a7ccb1a1e6
Owen <owenperedo@gmail.com>
2019-09-04 16:08:39 -0400

    Fixed typo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -134,7 +134,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-unless ENV[DISABLE_DATABASE_ENVIRONMENT_CHECK]
+unless ENV['DISABLE_DATABASE_ENVIRONMENT_CHECK']
   ActiveRecord::Migration.maintain_test_schema!
 end
 
