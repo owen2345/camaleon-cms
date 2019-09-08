34184ede4aad4fb7e1955685f43ab6f8f114a078
Brian Kephart <briantkephart@gmail.com>
2019-09-08 15:12:25 -0500

    Zeitwerk compatibility

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -7,7 +7,7 @@ require "camaleon_cms"
 
 module Dummy
   class Application < Rails::Application
-    config.autoloader = :classic
+    config.autoloader = :zeitwerk
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
