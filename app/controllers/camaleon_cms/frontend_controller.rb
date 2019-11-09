45921e9e9d1c1d96222534ae058617f88354684b
texpert <branzeanu.aurel@gmail.com>
2019-11-09 04:49:58 +0200

    Fixed the themes preview error.
    On the common site visit, the `current_theme` is instantiated using `current_site.get_theme.decorate` method chain.
    
    If the FrontendController receives the `ccc_theme_preview` param, the current_theme is instantiated from this param's value, but it is not used, because the `current_site.get_theme_slug` is being appended to the `lookup_context.prefixes`.
    
    So, given the correct behaviour of different methods of `current_theme` instantiation, the fix was to append `current_theme.name` instead of `current_site.get_theme_slug`  to the `lookup_context.prefixes`.

--- app/controllers/camaleon_cms/frontend_controller.rb
+++ app/controllers/camaleon_cms/frontend_controller.rb
@@ -243,7 +243,7 @@ class CamaleonCms::FrontendController < CamaleonCms::CamaleonController
     lookup_context.prefixes.delete_if{|t| t =~ /themes\/(.*)\/views/i || t == "camaleon_cms/default_theme" || t == "themes/#{current_site.id}/views" }
 
     lookup_context.prefixes.append("themes/#{current_site.id}/views") if Dir.exist?(Rails.root.join('app', 'apps', 'themes', current_site.id.to_s).to_s)
-    lookup_context.prefixes.append("themes/#{current_site.get_theme_slug}/views")
+    lookup_context.prefixes.append("themes/#{current_theme.name}/views")
     lookup_context.prefixes.append("camaleon_cms/default_theme")
 
     lookup_context.prefixes = lookup_context.prefixes.uniq
