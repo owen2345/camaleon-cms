07bbdff9a374cf9cfe87621539d3bf32e5c58e88
texpert <branzeanu.aurel@gmail.com>
2019-11-17 20:34:44 +0200

    Fix to require the themes assets for Dummy spec server, because it is not installed with copying themes.
    Removed unmaintained ruby 2.2 from Travis, replacing it with ruby 2.3, which has fine new features as safe navigation and squiggly heredoc. Also, removed rubocop version constrain, and set rubocop ruby target to 2.3.

--- spec/dummy/app/assets/config/manifest.js
+++ spec/dummy/app/assets/config/manifest.js
@@ -1,4 +1,7 @@
 //= link_tree ../images
 //= link_tree ../javascripts
 //= link_tree ../stylesheets
+//= link_tree ../../../../../app/apps/themes/default/assets
+//= link_tree ../../../../../app/apps/themes/camaleon_first/assets
+//= link_tree ../../../../../app/apps/themes/new/assets
 //= link camaleon-cms.js
