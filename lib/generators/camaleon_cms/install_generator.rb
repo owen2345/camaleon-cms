1387891a985d69fbbc976cc42ba6293bc6a9742b
Owen <owenperedo@gmail.com>
2020-01-07 15:32:03 -0400

    fix: for sprockets 4, generate manifest to precompile

--- lib/generators/camaleon_cms/install_generator.rb
+++ lib/generators/camaleon_cms/install_generator.rb
@@ -12,23 +12,28 @@ module CamaleonCms
         Dir.mkdir Rails.root.join("app", "apps").to_s unless Dir.exist?(Rails.root.join("app", "apps").to_s)
         directory("apps", "app/apps")
         directory( File.join($camaleon_engine_dir, 'app/apps/themes').to_s, 'app/apps/themes')
-        assets_config_dir = Rails.root.join('app', 'assets', 'config')
-        FileUtils.makedirs(assets_config_dir)
-        assets_config_file = assets_config_dir.join('manifest.js')
-        FileUtils.touch(assets_config_file) unless File.file?(assets_config_file)
-        append_to_file assets_config_file, <<~ASSETS
 
-                                             // Camaleon CMS assets
-                                             //= link camaleon-cms.js
-                                             //= link_tree ../../apps/themes/camaleon_first/assets
-                                             //= link_tree ../../apps/themes/default/assets
-                                             //= link_tree ../../apps/themes/new/assets
-                                           ASSETS
-        append_to_file Rails.root.join('config', 'initializers', 'assets.rb'),
-          <<~PRECOMPILE_MANIFEST
+        sprokects_3 = !defined?(Sprockets::BabelProcessor)
+        unless sprokects_3
+          assets_config_dir = Rails.root.join('app', 'assets', 'config')
+          FileUtils.makedirs(assets_config_dir)
+          assets_config_file = assets_config_dir.join('manifest.js')
+          FileUtils.touch(assets_config_file) unless File.file?(assets_config_file)
+          append_to_file assets_config_file, <<~ASSETS
+  
+                                               // Camaleon CMS assets
+                                               //= link camaleon-cms.js
+                                               //= link_tree ../../apps/themes/camaleon_first/assets
+                                               //= link_tree ../../apps/themes/default/assets
+                                               //= link_tree ../../apps/themes/new/assets
+                                             ASSETS
+          append_to_file Rails.root.join('config', 'initializers', 'assets.rb'),
+            <<~PRECOMPILE_MANIFEST
+  
+              Rails.application.config.assets.precompile += %w( manifest.js )
+            PRECOMPILE_MANIFEST
+        end
 
-            Rails.application.config.assets.precompile += %w( manifest.js )
-          PRECOMPILE_MANIFEST
         append_to_file 'Gemfile' do
           "\n\n#################### Camaleon CMS include all gems for plugins and themes #################### \nrequire './lib/plugin_routes' \ninstance_eval(PluginRoutes.draw_gems)"
         end
