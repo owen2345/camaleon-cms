519952eebc97e9387387046a860c316f3d47bc28
Brian Kephart <briantkephart@gmail.com>
2020-01-14 14:32:03 -0600

    compile TinyMCE assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,8 +3,6 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.tinymce.install = :copy
-
 # Add additional assets to the asset load path
 Rails.application.config.assets.precompile += %w( camaleon_cms/* )
 # Rails.application.config.assets.precompile += %w( themes/*/assets/* )
