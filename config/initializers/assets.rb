db15bbbb14b6ce9c362a3bcd2bc396f7bee39409
Owen <owenperedo@gmail.com>
2020-01-20 04:59:04 -0400

    feat: compile tinymce assets rather than copy

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.tinymce.install = :copy
+Rails.application.config.tinymce.install = :compile
 
 # Add additional assets to the asset load path
 Rails.application.config.assets.precompile += %w( camaleon_cms/* )
