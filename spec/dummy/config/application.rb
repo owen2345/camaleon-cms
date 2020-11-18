ca16b1d0a9fe3daa33d98a8e10c280f34cd648e6
Brian Kephart <briantkephart@gmail.com>
2020-11-17 18:44:45 -0600

    Rails 6.1 support

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,7 +1,19 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
-
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+# require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 require "camaleon_cms"
 
