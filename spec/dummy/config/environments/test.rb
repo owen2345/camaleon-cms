c70e8703c3247a0b5868d0ac490546d734fdd18c
texpert <branzeanu.aurel@gmail.com>
2021-10-11 14:04:14 +0300

    Do not check for precompiled assets when testing

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -20,6 +20,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.assets.check_precompiled_asset = false
+
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
