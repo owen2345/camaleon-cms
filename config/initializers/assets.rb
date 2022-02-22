3c92054b6e10040cc4b49786dec0901393b23405
Brian Kephart <briantkephart@gmail.com>
2022-02-21 20:57:10 -0600

    Use non-digest TinyMCE assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.tinymce.install = :compile
+Rails.application.config.tinymce.install = :copy
 
 # Add additional assets to the asset load path
 Rails.application.config.assets.precompile += %w( camaleon_cms/* )
