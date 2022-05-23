a9a9eb1239032ea761f023215686756f9dd0ebaf
texpert <branzeanu.aurel@gmail.com>
2022-05-23 10:48:15 +0300

    Fix sample jpeg file download to fetch a file from Wikimedia instead of tuzitio website, because the latter doesn't work and the specs are failing

--- spec/features/admin/media_spec.rb
+++ spec/features/admin/media_spec.rb
@@ -1,17 +1,20 @@
-require "rails_helper"
-describe "the Media", js: true do
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'the Media', js: true do
   init_site
 
-  it "list media" do
+  it 'list media' do
     admin_sign_in
     visit "#{cama_root_relative_path}/admin/media"
     within '#cama_media_gallery' do
-      execute_script("$('#cama_search_form .dropdown-toggle.btn').click()")
+      execute_script('$(\'#cama_search_form .dropdown-toggle.btn\').click()')
       find('.add_folder').click
     end
 
     within '#add_folder_form' do
-      fill_in "folder", with: "test_folder_created_by_testing"
+      fill_in 'folder', with: 'test_folder_created_by_testing'
       find('button[type="submit"]').click
       wait_for_ajax
     end
@@ -19,12 +22,12 @@ describe "the Media", js: true do
 
     within '#cama_media_gallery' do
       # access into inner folder
-      page.execute_script("$('#cama_media_gallery .folder_item[data-key=\"test_folder_created_by_testing\"]').click()")
+      page.execute_script('$(\'#cama_media_gallery .folder_item[data-key="test_folder_created_by_testing"]\').click()')
       wait_for_ajax
       # attach_file('cama-upload-files', Rails.root.join('config', 'system.json').to_s)
 
       within '#cama_media_external' do
-        fill_in "remote_file", with: "http://camaleon.tuzitio.com/media/132/slider/slider-camaleon.jpg"
+        fill_in 'remote_file', with: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Jpegvergroessert.jpg'
         find('button[type="submit"]').click
         wait_for_ajax
       end
@@ -33,18 +36,17 @@ describe "the Media", js: true do
     # expect(page).to have_content('slider-camaleon.jpg')
 
     # delete uploaded file
-    page.execute_script("$('#cama_media_gallery .file_item[data-key=\"slider_camaleon.jpg\"] .del_item').click()")
+    page.execute_script('$(\'#cama_media_gallery .file_item[data-key="slider_camaleon.jpg"] .del_item\').click()')
     confirm_dialog
     wait_for_ajax
 
     # back to root
-    page.execute_script("$('#cama_media_gallery .media_folder_breadcrumb a:first').click()")
+    page.execute_script('$(\'#cama_media_gallery .media_folder_breadcrumb a:first\').click()')
     wait_for_ajax
 
     # delete folder
-    page.execute_script("$('#cama_media_gallery .folder_item[data-key=\"test_folder_created_by_testing\"] .del_folder').click()")
+    page.execute_script('$(\'#cama_media_gallery .folder_item[data-key="test_folder_created_by_testing"] .del_folder\').click()')
     confirm_dialog
     wait_for_ajax
-
   end
 end
