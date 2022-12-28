dc366497f352ed6b5d55dd08aea926da685ef6c6
texpert <branzeanu.aurel@gmail.com>
2022-12-29 01:32:04 +0200

    Add ESLint to the CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,24 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 6
   },
+  ignorePatterns: [
+    'app/apps/themes/camaleon_first/assets/js/magnific.min.js',
+    'app/apps/themes/camaleon_first/assets/js/modernizr.custom.js',
+    'app/assets/javascripts/camaleon_cms/bootstrap.*',
+    'app/assets/javascripts/camaleon_cms/admin/_bootstrap*',
+    'app/assets/javascripts/camaleon_cms/admin/bootstrap*',
+    'app/assets/javascripts/camaleon_cms/admin/introjs/*',
+    'app/assets/javascripts/camaleon_cms/admin/_jquery*',
+    'app/assets/javascripts/camaleon_cms/admin/jquery*',
+    'app/assets/javascripts/camaleon_cms/admin/jquery_validate/*',
+    'app/assets/javascripts/camaleon_cms/admin/lte/*',
+    'app/assets/javascripts/camaleon_cms/admin/momentjs/*',
+    'app/assets/javascripts/camaleon_cms/admin/tageditor/*',
+    'app/assets/javascripts/camaleon_cms/admin/tinymce/*',
+    'app/assets/javascripts/camaleon_cms/admin/_underscore.js',
+    'app/assets/javascripts/camaleon_cms/admin/uploader/_cropper.*',
+    'app/assets/javascripts/camaleon_cms/admin/uploader/_jquery.*'
+  ],
   rules: {
     'space-before-function-paren': ['error', 'never'],
     curly: ['error', 'multi-or-nest']
