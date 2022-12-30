52cfd30e33d78c4938ff48f7a3c859b655388440
texpert <branzeanu.aurel@gmail.com>
2022-12-30 23:40:22 +0200

    Fix ESLint issues in the `plugins/visibility_post/assets/js/form.js`

--- app/apps/plugins/visibility_post/assets/js/form.js
+++ app/apps/plugins/visibility_post/assets/js/form.js
@@ -1,30 +1,35 @@
-jQuery(function($){
-    var panel = $("#panel-post-visibility");
-    var link_edit = panel.find(".edit-visibility").click(function(){
-        panel.find(".panel-options").removeClass("hidden").show();
-        link_edit.hide();
-        return false;
-    });
-    panel.find(".lnk_hide").click(function(){
-        panel.find(".panel-options").hide();
-        link_edit.show();
-        return false;
-    });
+/* eslint-env jquery */
+jQuery(function($) {
+  const panel = $('#panel-post-visibility')
+  const linkEdit = panel.find('.edit-visibility').click(function() {
+    panel.find('.panel-options').removeClass('hidden').show()
+    linkEdit.hide()
+    return false
+  })
 
-    panel.find("input[name='post[visibility]']").change(function(){
-        var label = $(this).closest("label");
-        panel.find(".visibility_label").html(label.text());
-        label.siblings("div").hide();
-        var rel_block = label.next().show();
-        
-        if($(this).val() == 'private') rel_block.find('input.visibility_private_group_item:first').addClass('required data-error-place-parent');
-        else panel.find('input.visibility_private_group_item:first').removeClass('required');
+  panel.find('.lnk_hide').click(function() {
+    panel.find('.panel-options').hide()
+    linkEdit.show()
+    return false
+  })
 
-        if($(this).val() == 'password') rel_block.find('input:text').addClass('required');
-        else panel.find('input.password_field_value').removeClass('required');
-        
-    }).filter(":checked").trigger("change");
+  panel.find("input[name='post[visibility]']").change(function() {
+    const label = $(this).closest('label')
+    panel.find('.visibility_label').html(label.text())
+    label.siblings('div').hide()
+    const relBlock = label.next().show()
 
-    var cal_input = $("#form-post").find('#published_from');
-    cal_input.datetimepicker({format: 'YYYY-MM-DD HH:mm'});
-});
+    if ($(this).val() === 'private')
+      relBlock.find('input.visibility_private_group_item:first').addClass('required data-error-place-parent')
+    else
+      panel.find('input.visibility_private_group_item:first').removeClass('required')
+
+    if ($(this).val() === 'password')
+      relBlock.find('input:text').addClass('required')
+    else
+      panel.find('input.password_field_value').removeClass('required')
+  }).filter(':checked').trigger('change')
+
+  const calInput = $('#form-post').find('#published_from')
+  calInput.datetimepicker({ format: 'YYYY-MM-DD HH:mm' })
+})
