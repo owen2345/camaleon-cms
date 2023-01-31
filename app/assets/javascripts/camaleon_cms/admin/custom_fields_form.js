e6bba39cf52cac9c11a2257fbd7595117158830b
Owen Peredo <owenperedo@gmail.com>
2023-01-31 06:46:23 -0400

    feat: refactor fields form

--- app/assets/javascripts/camaleon_cms/admin/custom_fields_form.js
+++ app/assets/javascripts/camaleon_cms/admin/custom_fields_form.js
@@ -1,7 +1,6 @@
 // add actions to assign custom fields to any model selected
 jQuery(function($){
   var panel = $("#cama_custom_field_form");
-  var group_class_name = panel.attr("data-group_class_name");
   var $content_fields = $( "#sortable-fields", panel);
   $content_fields.sortable({
     handle: ".panel-sortable"
@@ -28,19 +27,24 @@ jQuery(function($){
   panel.on("click", ".panel-delete", function(){
     var parent = $(this).parents(".item:first");
     if(confirm(I18n("msg.delete_item"))){
-      parent.remove()
+      var check = parent.find('input.destroy_check');
+      if (check.length) {
+          parent.hide();
+          check[0].checked = true;
+      } else
+        parent.remove();
     }
     return false;
   });
 
   $('#select_assign_group', panel).change(function(){
     var option = $(this).find('option:checked');
-    _change_additional_select(option);
     var txt_help = option.data('help');
+    togglePostsDropdown(option.val() === '_post_simple');
     if(txt_help) txt_help = '<div class="alert alert-info"><i class="fa fa-info-circle"></i>&nbsp; ' + txt_help + ' </div>';
     $('#select_assign_group_help', panel).html(txt_help);
     $('#select_assign_group_caption', panel).val(option.parent('optgroup').attr("label") + ' ' + option.text());
-  }).val(_search_group_class_name(group_class_name)).trigger('change');
+  });
 
   $('#select_post_simple', panel).change(function(){
     var option = $(this).find('option:checked');
@@ -48,34 +52,7 @@ jQuery(function($){
     if(txt_help) txt_help = '<div class="alert alert-info"><i class="fa fa-info-circle"></i>&nbsp; ' + txt_help + ' </div>';
     $('#select_assign_group_help', panel).html(txt_help);
     $('#select_assign_group_caption', panel).val(option.parent('optgroup').attr("label") + ': ' + option.text());
-  }).val(group_class_name).trigger('change');
-
-  $('#select_category_simple', panel).change(function(){
-    var option = $(this).find('option:checked');
-    var txt_help = option.data('help');
-    if(txt_help) txt_help = '<div class="alert alert-info"><i class="fa fa-info-circle"></i>&nbsp; ' + txt_help + ' </div>';
-    $('#select_assign_group_help', panel).html(txt_help);
-    $('#select_assign_group_caption', panel).val(option.parent('optgroup').attr("label") + ': ' + option.text());
-  }).val(group_class_name).trigger('change');
-
-  function _change_additional_select(option){
-    var option_value = option.attr('value'),
-      additional_options = ['_post_simple', '_category_simple'];
-
-    for (key in additional_options) {
-      if (option_value === additional_options[key]){
-        $('#select'+additional_options[key], panel).show().removeAttr('disabled');
-      }else{
-        $('#select'+additional_options[key], panel).hide().attr('disabled','disabled');
-      }
-    }
-  }
-
-  function _search_group_class_name(group_class_name) {
-    if (group_class_name.search("Post,") == 0) group_class_name = '_post_simple';
-    if (group_class_name.search("Category_Post,") == 0) group_class_name = '_category_simple';
-    return group_class_name;
-  }
+  });
 
   function cama_custom_field_set_slug(_panel){
     $('.text-slug:not(.runned)', _panel || panel).each(function(){
@@ -91,4 +68,20 @@ jQuery(function($){
       $(this).addClass('runned')
     });
   }
+
+  function applyCurrentSelected() {
+    var val = $('#select_assign_group', panel).attr('data-value');
+    var postOption = $('#select_post_simple', panel).find(`option[value='${val}']`).length;
+    var dropdown = $('#select_assign_group', panel);
+    postOption ? dropdown.val('_post_simple').trigger('change') : dropdown.val(val);
+    $('#select_post_simple', panel).val(val);
+  }
+
+  function togglePostsDropdown(flag) {
+    var dropdown = $('#select_post_simple', panel);
+    dropdown[0].disabled = !flag;
+    flag ? dropdown.show() : dropdown.hide();
+  }
+
+  applyCurrentSelected();
 });
