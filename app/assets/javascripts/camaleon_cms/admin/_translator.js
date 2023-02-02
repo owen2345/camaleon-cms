67ccc8cc51425e49a2b59012ef22f1acb32b9fc7
Brian Kephart <briantkephart@gmail.com>
2023-02-02 16:15:31 -0600

    Fix mistranslated for/in loops

--- app/assets/javascripts/camaleon_cms/admin/_translator.js
+++ app/assets/javascripts/camaleon_cms/admin/_translator.js
@@ -1,13 +1,13 @@
 /* eslint-env jquery */
 jQuery(function($) {
-/**
- * TRANSLATOR PLUGIN
- *
- * Example:
- * var trans = jQuery("input[type='text'], textarea").not(":hidden").Translatable(["es", "en"])  ===> convert input/textarea into translatable panel
- *
- * trans.trigger("trans_integrate") ==> encode translations into owner element (destroy translation elements)
- */
+  /**
+   * TRANSLATOR PLUGIN
+   *
+   * Example:
+   * var trans = jQuery("input[type='text'], textarea").not(":hidden").Translatable(["es", "en"])  ===> convert input/textarea into translatable panel
+   *
+   * trans.trigger("trans_integrate") ==> encode translations into owner element (destroy translation elements)
+   */
 
   /**
      *
@@ -32,13 +32,11 @@ jQuery(function($) {
       let res = ''
       /* eslint-disable-next-line eqeqeq */
       if (!text || text.trim().search('<!--') != 0) { // not translated string
-        languages.forEach(function(i) { translationsPerLocale[languages[i]] = text })
-
+        languages.forEach(function(lang) { translationsPerLocale[lang] = text })
         res = translationsPerLocale
       } else { // has translations
         const splitted = text.split('<!--:-->')
-        splitted.forEach(function(i) {
-          const str = splitted[i]
+        splitted.forEach(function(str) {
           const mAtch = str.trim().match(/^<!--:([\w||-]{2,5})/)
           if (mAtch && mAtch.length === 2) {
             mAtch[1] = mAtch[1].replace('--', '')
@@ -71,22 +69,22 @@ jQuery(function($) {
       const translations = getTranslations(ele.is('select') ? ele.data('value') : ele.val()); const inputs = {}
       const classGroup = ele.parent().hasClass('form-group') ? '' : 'form-group'
       // decoding languages
-      languages.forEach(function(ii) {
-        const l = languages[ii]
-        const key = 'translation-' + l + '-' + translatorCounter
+      languages.forEach(function(lang) {
+        const firstLang = lang === languages[0]
+        const key = 'translation-' + lang + '-' + translatorCounter
         tabsTitle.push(
           /* eslint-disable-next-line eqeqeq */
-          '<li role="presentation" class="pull-right ' + (ii == 0 ? 'active' : '') + '"><a href="#pane-' + key + '" role="tab" data-toggle="tab">' + l.titleize() + '</a></li>'
+          '<li role="presentation" class="pull-right ' + (firstLang ? 'active' : '') + '"><a href="#pane-' + key + '" role="tab" data-toggle="tab">' + lang.titleize() + '</a></li>'
         )
         const clone = ele.clone(true)
-          .attr({ id: key, name: key, 'data-name': key, 'data-translation_l': l, 'data-translation': 'translation-' + l })
-          .addClass('translate-item').val(getTranslation(translations, l))
-        if (ii > 0 && !clone.hasClass('required_all_langs'))
+          .attr({ id: key, name: key, 'data-name': key, 'data-translation_l': lang, 'data-translation': 'translation-' + lang })
+          .addClass('translate-item').val(getTranslation(translations, lang))
+        if (!firstLang && !clone.hasClass('required_all_langs'))
           clone.removeClass('required') // to permit enter empty values for secondary languages
-        inputs[l] = clone
+        inputs[lang] = clone
         clone.wrap(
           /* eslint-disable-next-line eqeqeq */
-          "<div class='tab-pane " + classGroup + ' trans_tab_item ' + (ii == 0 ? 'active' : '') + "' id='pane-" + key + "'/>"
+          "<div class='tab-pane " + classGroup + ' trans_tab_item ' + (firstLang ? 'active' : '') + "' id='pane-" + key + "'/>"
         )
         tabsContent.push(clone.parent())
         translatorCounter++
