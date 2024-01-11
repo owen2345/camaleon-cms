73934aaec998f359c1c5a6feb8b36b2596a12daa
Brian Kephart <briantkephart@gmail.com>
2024-01-11 17:25:09 -0600

    Extract 'yarn lint' command from CI

--- app/assets/javascripts/camaleon_cms/admin/nav_menu.js
+++ app/assets/javascripts/camaleon_cms/admin/nav_menu.js
@@ -1,5 +1,5 @@
 /* eslint-env jquery */
-$(function () {
+$(function() {
   const panel = $('#menu_content')
   const menuForm = $('#menu_form')
   const listPanel = $('#menus_list')
@@ -9,13 +9,13 @@ $(function () {
 
   // reorder items
   let lastData = { items: listPanel.nestable('serialize') }
-  listPanel.on('change', function () {
+  listPanel.on('change', function() {
     const data = { items: listPanel.nestable('serialize') }
     if (JSON.stringify(lastData) === JSON.stringify(data))
       return
 
     panel.find('#menu_reoreder_loading').show()
-    $.post(listPanel.attr('data-reorder_url'), data, function (res) {
+    $.post(listPanel.attr('data-reorder_url'), data, function(res) {
       lastData = data
       panel.find('#menu_reoreder_loading').hide()
       if (res)
@@ -24,20 +24,20 @@ $(function () {
   })
 
   // save a new menu item into DB
-  const saveMenu = function (data) {
+  const saveMenu = function(data) {
     showLoading()
 
-    $.post(listPanel.attr('data-url'), data, function (res) {
+    $.post(listPanel.attr('data-url'), data, function(res) {
       listPanel.children('.dd-list').append($(res).children())
       hideLoading()
     })
   }
 
   // add menu items (non-external)
-  menuItemsAvailable.find('.add_links_to_menu').click(function () {
+  menuItemsAvailable.find('.add_links_to_menu').click(function() {
     const data = { items: [], authenticity_token: menuForm.find('[name="authenticity_token"]').val() }
     let flag = false
-    $(this).closest('.panel').find('input:checkbox:checked').each(function () {
+    $(this).closest('.panel').find('input:checkbox:checked').each(function() {
       flag = true
       data.items.push({ id: $(this).val(), kind: $(this).closest('.class_type').attr('data-type') })
     }).prop('checked', false)
@@ -50,10 +50,10 @@ $(function () {
   })
 
   // add custom menu items (non-external)
-  menuItemsAvailable.find('.add_links_custom_to_menu').click(function () {
+  menuItemsAvailable.find('.add_links_custom_to_menu').click(function() {
     const data = { custom_items: [], authenticity_token: menuForm.find('[name="authenticity_token"]').val() }
     let flag = false
-    $(this).closest('.panel').find('input:checkbox:checked').each(function () {
+    $(this).closest('.panel').find('input:checkbox:checked').each(function() {
       flag = true
       data.custom_items.push({ url: $(this).val(), kind: $(this).attr('data-kind'), label: $(this).attr('data-label') })
     }).prop('checked', false)
@@ -66,7 +66,7 @@ $(function () {
   })
 
   // add external link
-  menuItemsAvailable.find('.form-custom-link').submit(function () {
+  menuItemsAvailable.find('.form-custom-link').submit(function() {
     const form = $(this)
     if (!form.valid())
       return false
@@ -78,7 +78,7 @@ $(function () {
   })
 
   // edit external menu items
-  listPanel.on('click', '.item_external', function () {
+  listPanel.on('click', '.item_external', function() {
     const link = $(this)
     open_modal({
       title: link.attr('data-original-title') || link.attr('title'),
@@ -87,12 +87,12 @@ $(function () {
       callback: (modal) => {
         const form = modal.find('form')
         init_form_validations(form)
-        form.submit(function () {
+        form.submit(function() {
           if (!form.valid())
             return false
 
           showLoading()
-          $.post(form.attr('action'), form.serialize(), function (res) {
+          $.post(form.attr('action'), form.serialize(), function(res) {
             link.closest('li').replaceWith($(res).html())
             modal.modal('hide')
             hideLoading()
@@ -105,13 +105,13 @@ $(function () {
   })
 
   // delete menu items
-  listPanel.on('click', '.delete_menu_item', function () {
+  listPanel.on('click', '.delete_menu_item', function() {
     const link = $(this)
     if (!confirm(I18n('msg.confirm_del', 'Are you sure to delete this item?')))
       return false
 
     showLoading()
-    $.get(link.attr('href'), function () {
+    $.get(link.attr('href'), function() {
       link.closest('.dd-item').remove()
       hideLoading()
     })
@@ -128,7 +128,7 @@ $(function () {
   })
 
   // menus list - change dropdown
-  panel.find('#menu_items #switch_nav_menu_form select').change(function () {
+  panel.find('#menu_items #switch_nav_menu_form select').change(function() {
     if (!$(this).val())
       return
 
@@ -136,7 +136,7 @@ $(function () {
   })
 
   // custom fields
-  listPanel.on('click', '.custom_settings_link', function () {
+  listPanel.on('click', '.custom_settings_link', function() {
     const link = $(this)
     open_modal({
       title: link.attr('data-original-title') || link.attr('title'),
@@ -145,12 +145,12 @@ $(function () {
       callback: (modal) => {
         const form = modal.find('form')
         init_form_validations(form)
-        form.submit(function () {
+        form.submit(function() {
           if (!form.valid())
             return false
 
           showLoading()
-          $.post(form.attr('action'), form.serialize(), function (res) {
+          $.post(form.attr('action'), form.serialize(), function(res) {
             if (res)
               alert(res)
 
