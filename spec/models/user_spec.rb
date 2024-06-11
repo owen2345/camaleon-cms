824fd11a6f751c21ceadbcbaa04312d0c3c31316
texpert <branzeanu.aurel@gmail.com>
2024-06-11 20:16:55 +0300

    Fix rubocop errors RSpec/NoExpectationExample: No expectation found in this example in models/user_spec
    `

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe CamaleonCms::User, type: :model do
   describe 'email' do
     it 'is lowercased' do
-      user = described_class.create!(
-        email: 'FOO@BAR.COM',
-        username: 'test', password: 'test'
-      )
-      user.email.should == 'foo@bar.com'
+      user = described_class.create!(email: 'FOO@BAR.COM', username: 'test', password: 'test')
+
+      expect(user.email).to eql('foo@bar.com')
     end
   end
 end
