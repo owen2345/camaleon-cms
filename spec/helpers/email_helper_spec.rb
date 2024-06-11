adba1563c0561e90c93fd415fbe59a4397466997
texpert <branzeanu.aurel@gmail.com>
2024-06-11 20:18:03 +0300

    Fix rubocop errors RSpec/ClassCheck: Prefer be_a over be_a_kind_of in helpers/email_helper_spec
    `

--- spec/helpers/email_helper_spec.rb
+++ spec/helpers/email_helper_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'CamaleonCms::EmailHelper' do
@@ -6,7 +8,7 @@ describe 'CamaleonCms::EmailHelper' do
   describe '#send_email (old way)' do
     it 'returns empty' do
       expect(helper.send_email('test@gmail.com', 'Test Email', 'test content', 'owenperedo@gmail.com'))
-        .to be_a_kind_of(ActionMailer::MailDeliveryJob)
+        .to be_a(ActionMailer::MailDeliveryJob)
     end
   end
 
@@ -15,7 +17,7 @@ describe 'CamaleonCms::EmailHelper' do
       expect(
         helper.cama_send_email('test@gmail.com', 'Test Email',
                                { content: 'test content', from: 'owenperedo@gmail.com' })
-      ).to be_a_kind_of(ActionMailer::MailDeliveryJob)
+      ).to be_a(ActionMailer::MailDeliveryJob)
     end
   end
 end
