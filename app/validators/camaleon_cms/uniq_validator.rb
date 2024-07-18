e89ccd4c46fb8f8556a3e6682345cc9875ded79d
texpert <branzeanu.aurel@gmail.com>
2024-07-18 13:15:36 +0300

    Fix more rubocop issues

--- app/validators/camaleon_cms/uniq_validator.rb
+++ app/validators/camaleon_cms/uniq_validator.rb
@@ -3,9 +3,15 @@ module CamaleonCms
     def validate(record)
       return if record.skip_slug_validation?
 
-      if CamaleonCms::TermTaxonomy.where(slug: record.slug).where.not(id: record.id).where("#{CamaleonCms::TermTaxonomy.table_name}.taxonomy" => record.taxonomy).where("#{CamaleonCms::TermTaxonomy.table_name}.parent_id" => record.parent_id).size.positive?
-        record.errors[:base] << I18n.t('camaleon_cms.admin.post.message.requires_different_slug').to_s
-      end
+      taxonomy_table = CamaleonCms::TermTaxonomy.table_name
+      slug_exists = CamaleonCms::TermTaxonomy.where(slug: record.slug)
+                                             .where.not(id: record.id)
+                                             .where("#{taxonomy_table}.taxonomy" => record.taxonomy)
+                                             .where("#{taxonomy_table}.parent_id" => record.parent_id).exists?
+
+      return unless slug_exists
+
+      record.errors[:base] << I18n.t('camaleon_cms.admin.post.message.requires_different_slug').to_s
     end
   end
 end
