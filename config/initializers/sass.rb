968860fa841b08db28f194d3bcfe725197f30c69
texpert <branzeanu.aurel@gmail.com>
2024-08-05 20:41:21 +0300

    Refactor Sprockets::Function to SassC namespace, which is present in dartsass-sprockets

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,3 +1,5 @@
+require 'sassc'
+
 module Sprockets
   # Processor engine class for the SASS/SCSS compiler. Depends on the `sass` gem.
   #
@@ -11,23 +13,23 @@ module Sprockets
       # return them path (this prefix automatically the path with current theme location)
       # Sample: .container{ background: #ffffff url(asset_theme_path('img/patterns/pattern1.jpg')); }
       def asset_theme_path(path, options = {})
-        asset_path(Autoload::Sass::Script::String.new("#{get_theme_prefix}/#{path.value}".gsub('//', '/')), options)
+        asset_path(::SassC::Script::Value::String.new("#{get_theme_prefix}/#{path.value}".gsub('//', '/')), options)
       end
 
       # return them path (this prefix automatically the path with current theme location)
       # Sample: .container{ background: #ffffff asset-theme-url('img/patterns/pattern1.jpg'); }
       def asset_theme_url(path, options = {})
-        asset_url(Autoload::Sass::Script::String.new("#{get_theme_prefix}/#{path.value}".gsub('//', '/')), options)
+        asset_url(::SassC::Script::Value::String.new("#{get_theme_prefix}/#{path.value}".gsub('//', '/')), options)
       end
 
       # return them path (this prefix automatically the path with current theme location)
       def asset_plugin_path(path, options = {})
-        asset_path(Autoload::Sass::Script::String.new("#{get_plugin_prefix}/#{path.value}"), options)
+        asset_path(::SassC::Script::Value::String.new("#{get_plugin_prefix}/#{path.value}"), options)
       end
 
       # return them path (this prefix automatically the path with current theme location)
       def asset_plugin_url(path, options = {})
-        asset_url(Autoload::Sass::Script::String.new("#{get_plugin_prefix}/#{path.value}"), options)
+        asset_url(::SassC::Script::Value::String.new("#{get_plugin_prefix}/#{path.value}"), options)
       end
 
       private
@@ -35,23 +37,17 @@ module Sprockets
       # get plugin asset prefix
       def get_plugin_prefix
         file = options[:filename]
-        res = ''
-        if file.include?('/plugins/')
-          res = "plugins/#{file.split('/plugins/').last.split('/').first}/#{if file.include?('apps/plugins/')
-                                                                              'assets/'
-                                                                            end}"
-        end
-        res
+        return '' unless file.include?('/plugins/')
+
+        "plugins/#{file.split('/plugins/').last.split('/').first}/#{file.include?('apps/plugins/') ? 'assets/' : ''}"
       end
 
       # get theme asset prefix
       def get_theme_prefix
         file = options[:filename]
-        res = ''
-        if file.include?('/themes/')
-          res = "themes/#{file.split('/themes/').last.split('/').first}/#{'assets/' if file.include?('apps/themes/')}"
-        end
-        res
+        return '' unless file.include?('/themes/')
+
+        "themes/#{file.split('/themes/').last.split('/').first}/#{'assets/' if file.include?('apps/themes/')}"
       end
     end
   end
