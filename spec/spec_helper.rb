b763b065ef5229046f92a213569725dd85c7fd99
texpert <branzeanu.aurel@gmail.com>
2025-03-13 01:47:06 +0200

    Try to point Chrome's user-data-dir to Rails.root.join('tmp') - to fix the 'session not created: DevToolsActivePort file doesn't exist' error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,7 @@ Capybara.register_driver :selenium_chrome125 do |app|
   browser_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
     opts.add_argument('--disable-site-isolation-trials')
+    opts.add_argument("--user-data-dir=#{Rails.root.join('tmp')}")
   end
 
   browser_options.add_option(:browser_version, '125')
@@ -35,6 +36,7 @@ Capybara.register_driver :selenium_chrome_headless125 do |app|
     opts.add_argument('--disable-gpu') if Gem.win_platform?
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
     opts.add_argument('--disable-site-isolation-trials')
+    opts.add_argument("--user-data-dir=#{Rails.root.join('tmp')}")
   end
 
   browser_options.add_option(:browser_version, '125')
