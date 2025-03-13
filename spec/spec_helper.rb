36e18126e6102973f06340b5d51e42d335432f63
texpert <branzeanu.aurel@gmail.com>
2025-03-13 02:02:54 +0200

    Add --no-sandbox to selenium chrome driver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,7 @@ Capybara.register_driver :selenium_chrome125 do |app|
   browser_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
     opts.add_argument('--disable-site-isolation-trials')
+    opts.add_argument('--no-sandbox')
     opts.add_argument("--user-data-dir=#{Rails.root.join('tmp')}")
   end
 
@@ -36,6 +37,7 @@ Capybara.register_driver :selenium_chrome_headless125 do |app|
     opts.add_argument('--disable-gpu') if Gem.win_platform?
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
     opts.add_argument('--disable-site-isolation-trials')
+    opts.add_argument('--no-sandbox')
     opts.add_argument("--user-data-dir=#{Rails.root.join('tmp')}")
   end
 
